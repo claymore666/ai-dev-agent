1725c39d8cbdfeda3007a9015adf05a0917fa1c6
Your GitHub Username <your-github-email@example.com>
2025-03-14 19:32:04 +0000

    cleanup Jenkins

--- jenkins/agent/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Activate Python virtual environment
-source /home/jenkins/venv/bin/activate
-
-# Pass all arguments to the jenkins-agent entrypoint
-exec /usr/local/bin/jenkins-agent "$@"
